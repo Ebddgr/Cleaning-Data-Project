4a1ebffbbfb6b3095477d6953c9810761fe774b0
Jane Doe <jane@example.org>
2022-02-08 18:54:43 +0330

    upload

--- run_analysis.R
+++ run_analysis.R
@@ -1,1 +1,8 @@
-hi
+library(tidyverse)
+#Create Data frame from test folder
+test_files <- list.files("UCI HAR Dataset/test",recursive = T,full.names = T)
+test_data <- lapply(test_files,read_tsv) %>% bind_cols()
+
+##change the column names
+names(test_data) <- str_remove_all(test_files,".*[/]|.txt")
+
